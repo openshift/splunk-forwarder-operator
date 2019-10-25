5c1d717d5e57208254597f5c4bea683d2462b497
Wesley Hearn <whearn@redhat.com>
2019-10-25 14:02:09 -0400

    Remove the pod watchers

--- pkg/controller/splunkforwarder/splunkforwarder_controller.go
+++ pkg/controller/splunkforwarder/splunkforwarder_controller.go
@@ -53,16 +53,6 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner SplunkForwarder
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &sfv1alpha1.SplunkForwarder{},
-	})
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
