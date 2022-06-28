a70ed6eb50d0f54c8a3915c57070dc7695265a6c
Karthik Perumal <kramraja@redhat.com>
2022-06-29 09:48:55 +1000

    Address gosec warning [OSD-11470]

--- config/config.go
+++ config/config.go
@@ -4,5 +4,5 @@ const (
 	OperatorName      string = "splunk-forwarder-operator"
 	OperatorNamespace string = "splunk-forwarder-operator"
 
-	SplunkAuthSecretName string = "splunk-auth"
+	SplunkAuthSecretName string = "splunk-auth" // #nosec G101 -- This is a false positive
 )
