7752b8da38d84371feef89bceb45afe944ec5818
Ritu Mundhe <rmundhe@redhat.com>
2023-12-01 10:38:21 -0600

    new line

--- pkg/kube/constants.go
+++ pkg/kube/constants.go
@@ -1,5 +1,5 @@
 package kube
 
 const (
-    MaxEventSize = 100 * 1024 // 100KB per k8s.io/kubernetes/apiserver/pkg/server/options/audit.go
+	MaxEventSize = 100 * 1024 // 100KB per k8s.io/kubernetes/apiserver/pkg/server/options/audit.go
 )
