5b47e75874fabbf29a2a7ade81b1401e6b7adca8
Wesley Hearn <whearn@redhat.com>
2025-10-15 11:37:25 -0400

    Reapply "Split off generate proxy config into its own function for easier unit testing, created unit tests around generating proxy configmaps, Replaced error checking in the controller with is not found and then requeued the reconcile on any other error"
    This reverts commit 236637b7003698fac5d2c784c179da3e5a15328f.

--- pkg/kube/volumemounts_test.go
+++ pkg/kube/volumemounts_test.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 	"testing"
 
+	configv1 "github.com/openshift/api/config/v1"
 	sfv1alpha1 "github.com/openshift/splunk-forwarder-operator/api/v1alpha1"
 	"github.com/openshift/splunk-forwarder-operator/config"
 	corev1 "k8s.io/api/core/v1"
@@ -14,6 +15,7 @@ func TestGetVolumeMounts(t *testing.T) {
 	type args struct {
 		instance    *sfv1alpha1.SplunkForwarder
 		useHECToken bool
+		proxyConfig *configv1.Proxy
 	}
 	var mountPropagationMode = corev1.MountPropagationHostToContainer
 	tests := []struct {
@@ -28,8 +30,7 @@ func TestGetVolumeMounts(t *testing.T) {
 					ObjectMeta: metav1.ObjectMeta{
 						Name: "test",
 					},
-					Spec: sfv1alpha1.SplunkForwarderSpec{
-					},
+					Spec: sfv1alpha1.SplunkForwarderSpec{},
 				},
 			},
 			want: []corev1.VolumeMount{
@@ -72,10 +73,105 @@ func TestGetVolumeMounts(t *testing.T) {
 					ObjectMeta: metav1.ObjectMeta{
 						Name: "test",
 					},
-					Spec: sfv1alpha1.SplunkForwarderSpec{
-					},
+					Spec: sfv1alpha1.SplunkForwarderSpec{},
 				},
 				useHECToken: true,
+			},
+			want: []corev1.VolumeMount{
+				{
+					Name:      "splunk-config",
+					MountPath: "/opt/splunkforwarder/etc/system/local",
+				},
+				{
+					Name:      "osd-monitored-logs-local",
+					MountPath: "/opt/splunkforwarder/etc/apps/osd_monitored_logs/local",
+				},
+				{
+					Name:      "osd-monitored-logs-metadata",
+					MountPath: "/opt/splunkforwarder/etc/apps/osd_monitored_logs/metadata",
+				},
+				{
+					Name:      "splunk-state",
+					MountPath: "/opt/splunkforwarder/var/lib",
+				},
+				{
+					Name:             "host",
+					MountPath:        "/host",
+					MountPropagation: &mountPropagationMode,
+					ReadOnly:         true,
+				},
+			},
+		},
+		{
+			name: "Use HEC Token config with proxy",
+			args: args{
+				instance: &sfv1alpha1.SplunkForwarder{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test",
+					},
+					Spec: sfv1alpha1.SplunkForwarderSpec{},
+				},
+				useHECToken: true,
+				proxyConfig: &configv1.Proxy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "cluster",
+					},
+					Spec: configv1.ProxySpec{
+						HTTPProxy:  "",
+						HTTPSProxy: "example.com",
+						NoProxy:    "127.0.0.1",
+					},
+				},
+			},
+			want: []corev1.VolumeMount{
+				{
+					Name:      "splunk-config",
+					MountPath: "/opt/splunkforwarder/etc/system/local",
+				},
+				{
+					Name:      "test-proxy",
+					MountPath: "/opt/splunkforwarder/etc/system/local",
+				},
+				{
+					Name:      "osd-monitored-logs-local",
+					MountPath: "/opt/splunkforwarder/etc/apps/osd_monitored_logs/local",
+				},
+				{
+					Name:      "osd-monitored-logs-metadata",
+					MountPath: "/opt/splunkforwarder/etc/apps/osd_monitored_logs/metadata",
+				},
+				{
+					Name:      "splunk-state",
+					MountPath: "/opt/splunkforwarder/var/lib",
+				},
+				{
+					Name:             "host",
+					MountPath:        "/host",
+					MountPropagation: &mountPropagationMode,
+					ReadOnly:         true,
+				},
+			},
+		},
+		{
+			name: "Use HEC Token config with empty proxy",
+			args: args{
+				instance: &sfv1alpha1.SplunkForwarder{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test",
+					},
+					Spec: sfv1alpha1.SplunkForwarderSpec{},
+				},
+				useHECToken: true,
+				proxyConfig: &configv1.Proxy{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "cluster",
+					},
+					Spec: configv1.ProxySpec{
+						HTTPProxy:  "",
+						HTTPSProxy: "",
+						NoProxy:    "",
+					},
+				},
 			},
 			want: []corev1.VolumeMount{
 				{
@@ -105,7 +201,7 @@ func TestGetVolumeMounts(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GetVolumeMounts(tt.args.instance, tt.args.useHECToken); !reflect.DeepEqual(got, tt.want) {
+			if got := GetVolumeMounts(tt.args.instance, tt.args.useHECToken, tt.args.proxyConfig); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetVolumeMounts() = %v, want %v", got, tt.want)
 			}
 		})
